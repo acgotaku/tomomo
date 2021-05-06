4ecf0b898f7bd4463d054fea4ace194f521ba84d
acgotaku <acgotaku311@gmail.com>
2021-05-06 22:11:01 +0900

    add styles

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-mixins': {},
+    'postcss-import': {},
+    'postcss-nested': {}
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 import { i18n } from '@/i18n';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import router from '@/router';
+import store from '@/store';
+import '@/registerServiceWorker';
+import '@/styles/index.css';
 
 Vue.config.productionTip = false;
 
